3fd57f022797e0af756d7622e5996084843d4df9
Christian Gil Alaan <christiangilalaan@gmail.com>
2025-10-01 10:15:09 +0800

    rollback

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  // if deploying to vercel, use '/' else '/my-react-portfolio/'
-  base: process.env.VERCEL ? '/' : '/my-react-portfolio/',
+  base:"/my-react-portfolio/",
   plugins: [react(), tailwindcss()],
 })
